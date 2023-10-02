611f761456a93ed20b9ad6e6a80bd847ba58063c
RAISUL77 <139850058+RAISUL77@users.noreply.github.com>
2023-10-02 11:39:19 +0600

    Add files via upload

---
+++ problem2/ThreadSafeStack.java
@@ -0,0 +1,21 @@
+package problem2;
+import java.util.LinkedList;
+
+public class ThreadSafeStack {
+    private LinkedList<Integer> stack = new LinkedList<>();
+
+    public synchronized void push(Integer item) {
+        stack.push(item);
+    }
+
+    public synchronized Integer pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack.pop();
+    }
+
+    public synchronized boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
